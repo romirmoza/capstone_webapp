fe1123bec28422914c06f352a4df619a0ce98245
Romir Moza <romir.moza@gmail.com>
2019-08-09 12:30:14 -0500

    Moved static files

--- capstone_webapp/settings.py
+++ capstone_webapp/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = ''
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
@@ -129,7 +130,7 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+         os.path.join(BASE_DIR, "prelim", "static")
 ]
 
 
